cc2901ee0e98e6a4278c236727f830294a865287
Enitanayo <adeolaenitan98@gmail.com>
2025-02-24 11:51:03 +0100

    Added model training component

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -12,17 +12,24 @@ from src.components.data_transformation import data_transformation
 from src.components.data_transformation import data_transformation_config
 
 
+from src.components.model_trainer import model_trainer
+from src.components.model_trainer import model_trainer_config
+
+
+
 #Data ingestion Config class
 @dataclass
 class data_ingestion_config:
+    #creating path string such as artifacts/train.csv
     train_data_path:str= os.path.join('artifacts', 'train.csv')
     test_data_path:str= os.path.join('artifacts', 'test.csv')
     raw_data_path:str= os.path.join('artifacts', 'data.csv')
 
 
-# Dqata ingestion class
+# Data ingestion class
 class data_ingestion:
     def __init__(self):
+        # brings in the path string into this class 
         self.data_ingestion_config=data_ingestion_config()
     # initiate data ingestion
     def initiate_data_ingestion(self):
@@ -30,14 +37,16 @@ class data_ingestion:
             filepath = 'notebook\data\stud.csv'
             df = pd.read_csv(filepath)
             logging.info('Read the dataset as dataframe')
+            # creates only the artifact directory from artifacts/data.csv
             os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path), exist_ok=True)
-            
+            #saves the read dataframe to artifacts/data.csv
             df.to_csv(self.data_ingestion_config.raw_data_path, index=False, header=True)
             
             #train test split
             logging.info("Train test split initiated")
             train_set, test_set= train_test_split(df, test_size=0.2, random_state=42)
             
+            #saves the split dataframe into artifacts/train, test.csv respectively
             train_set.to_csv(self.data_ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.data_ingestion_config.test_data_path, index=False, header=True)
             
@@ -57,6 +66,9 @@ if __name__ == '__main__':
     train_data, test_data = obj.initiate_data_ingestion()
     
     dataTransformation = data_transformation()
-    dataTransformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr, _ = dataTransformation.initiate_data_transformation(train_data, test_data)
+    
+    modelTrainer = model_trainer()
+    print(modelTrainer.initiate_model_trainer(train_array=train_arr, test_array=test_arr))
     
     
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 import pickle
 
 from src.exception import CustomException
-
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -13,5 +13,26 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             pickle.dump(obj, file_obj)
             
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def evaluate_models(X_train, y_train ,X_test,  y_test, models):
+    try:
+        report = {}
+        
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            
+            pred_train_val = model.predict(X_train)
+            pred_test_val = model.predict(X_test)
+            
+            r2_pred_train_val = r2_score(y_train, pred_train_val)
+            r2_pred_test_val = r2_score(y_test, pred_test_val)
+            
+            report[list(models.keys())[i]] = r2_pred_test_val
+            
+        return report
     except Exception as e:
         raise CustomException(e, sys)
