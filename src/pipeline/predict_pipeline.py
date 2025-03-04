c8539338cb982d0cd541e808cbd2fc22c2deee06
Enitanayo <adeolaenitan98@gmail.com>
2025-03-04 14:55:47 +0100

    Used flask to create UI for prediction

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -8,15 +8,18 @@ class PredictPipeline:
     def __init__(self):
         pass
     
-    def predict():
-        model_path = os.path.join('artifacts', 'model.pkl')
-        preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
-        model = load_object(file_path=model_path)
-        preprocessor = load_object(file_path=preprocessor_path)
-        data_scaled=preprocessor.transform(features)
-        preds = model.predict(data_scaled)
-        
-        return preds
+    def predict(self, features):
+        try:
+            model_path = os.path.join('artifacts', 'model.pkl')
+            preprocessor_path = os.path.join('artifacts', 'preprocessor.pkl')
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled=preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
     
 class CustomData:
     def __init__(self,
@@ -42,20 +45,18 @@ class CustomData:
 
         self.writing_score = writing_score
         
-        def load_as_dataframe(self):
-            try:
-                custom_data_input_dict = {
-                    'gender' : self.gender,
-                    'race_ethnicity' : self.race_ethnicity,
-                    'parental_level_of_education' : self.parental_level_of_education,
-                    'lunch' : self.lunch,
-                    'test_preparation_course' : self.test_preparation_course,
-                    'reading_score' : self.reading_score,
-                    'writing_score' : self.writing_score
-                }
+    def load_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'gender' : [self.gender],
+                'race_ethnicity' : [self.race_ethnicity],
+                'parental_level_of_education' : [self.parental_level_of_education],
+                'lunch' : [self.lunch],
+                'test_preparation_course' : [self.test_preparation_course],
+                'reading_score' : [self.reading_score],
+                'writing_score' : [self.writing_score]
+            }
                 
-                return pd.DataFrame(custom_data_input_dict)
-            except Exception as e:
-                raise CustomException(e, sys)
-                 
-        
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
